a51c05cb45d2c49ae09c78d260ebd74df0e690d7
8Seph <evem-w@outlook.com>
2020-04-15 18:32:01 +0300

    Update Product.java

--- src/main/java/ru/geekbrains/persist/Product.java
+++ src/main/java/ru/geekbrains/persist/Product.java
@@ -1,15 +1,23 @@
 package ru.geekbrains.persist;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Table(name = "products")
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String name;
 
+    @Column
     private String description;
 
+    @Column
     private BigDecimal price;
 
     public Product() {
